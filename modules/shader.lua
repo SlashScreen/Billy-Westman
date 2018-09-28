e3f5b36cdf967360ed5dfdacdaaaa8874f06d5ec
GJ <TALLESTTOWER1@GMAIL.COM>
2018-09-28 16:10:55 -0700

    New shader file w/ create gradient function, gradShader. TODO: lerp

---
+++ modules/shader.lua
@@ -0,0 +1,26 @@
+--shader.lua
+
+function shader:constructGradient(r1,b1,g1,r2,b2,g2)
+  gradient = {}
+  gradient[color1][r] = r1
+  gradient[color1][g] = g1
+  gradient[color1][b] = b1
+  gradient[color2][r] = r2
+  gradient[color2][g] = g2
+  gradient[color2][b] = b2
+  return gradient
+end
+
+function shader:gradShader (gradient)
+  local gradeffect = love.graphics.newShader[[
+    vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
+      vec4 pixel = Texel(texture, texture_coords );//This is the current pixel color
+      number average = (pixel.r+pixel.b+pixel.g)/3.0;
+      pixel.r = average;
+      pixel.g = average;
+      pixel.b = average;
+      return pixel;
+    }
+  ]]
+  return gradeffect
+end
