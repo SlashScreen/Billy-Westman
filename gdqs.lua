a154e35b5a3064daebbab8908940ef6ba3038379
GJ <TALLESTTOWER1@GMAIL.COM>
2018-09-29 12:41:32 -0700

    removed todo

--- gdqs.lua
+++ gdqs.lua
@@ -15,7 +15,7 @@ function gdqsworld:load()
   gdqsworld.changemapConditionsMet = 0;
   gdqsworld.go_to = "test"
 
-  gradient = gdshader:constructGradient({["r"]=gdshader:toOneBase(38),["g"]=gdshader:toOneBase(255),["b"]=gdshader:toOneBase(10)},{["r"]=gdshader:toOneBase(211),["g"]=gdshader:toOneBase(0),["b"]=gdshader:toOneBase(0)})
+  gradient = gdshader:constructGradient({["r"]=gdshader:toOneBase(128),["g"]=gdshader:toOneBase(255),["b"]=gdshader:toOneBase(0)},{["r"]=gdshader:toOneBase(255),["g"]=gdshader:toOneBase(0),["b"]=gdshader:toOneBase(0)})
   shader = gdshader:gradShader(gradient)
 
   player = require "modules/player_module";
